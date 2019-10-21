01640f1c2fabb193384e0ab2cc54082f3ce2496d
v.turkov <v.turkov@sysolutions.ru>
2019-10-21 10:25:32 +0700

    The problem of a large number of requests has been fixed.

--- src/app/map-wrapper/components/access-point-layer.ts
+++ src/app/map-wrapper/components/access-point-layer.ts
@@ -39,6 +39,7 @@ export default abstract class AccessPointLayer<T extends AccessPoint> extends Ma
 
       this.on('add', () => {
         if (map.getZoom() >= this.maxZoom) {
+          this.startUpdateSwitch.emit(false);
           this.startUpdateSwitch.emit(true);
         } else {
           this.clearLayer();
@@ -51,6 +52,7 @@ export default abstract class AccessPointLayer<T extends AccessPoint> extends Ma
 
       map.on('moveend', () => {
         if (map.hasLayer(this) && map.getZoom() >= this.maxZoom) {
+          this.startUpdateSwitch.emit(false);
           this.startUpdateSwitch.emit(true);
         } else {
           this.clearLayer();
