fd52b7bcaed38311f64cd1ae3dae9f48d5ca8477
andrew47i <i329247@gmail.com>
2019-11-22 13:12:35 +0700

    страница тех. возможности

--- src/app/modules/pivot-table/components/technical-capabilities/technical-capabilities.component.ts
+++ src/app/modules/pivot-table/components/technical-capabilities/technical-capabilities.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-technical-capabilities',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TechnicalCapabilitiesComponent implements OnInit {
 
-  constructor() { }
+  tcForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.tcForm = fb.group({});
+  }
 
   ngOnInit() {
   }
