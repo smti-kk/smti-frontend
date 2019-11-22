96fd5a69e1d306b1ad967a4296b63afd216ea457
v.turkov <v.turkov@sysolutions.ru>
2019-11-22 12:34:14 +0700

    feat(filter-btns): filter btn as reactive form component

---
+++ src/app/modules/pivot-table/components/technical-capabilities/technical-capabilities.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TechnicalCapabilitiesComponent } from './technical-capabilities.component';
+
+describe('TechnicalCapabilitiesComponent', () => {
+  let component: TechnicalCapabilitiesComponent;
+  let fixture: ComponentFixture<TechnicalCapabilitiesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TechnicalCapabilitiesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TechnicalCapabilitiesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/pivot-table/components/technical-capabilities/technical-capabilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-capabilities',
+  templateUrl: './technical-capabilities.component.html',
+  styleUrls: ['./technical-capabilities.component.scss']
+})
+export class TechnicalCapabilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
