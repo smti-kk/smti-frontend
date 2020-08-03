dd897a0f379b0f3a7b878ba93b1594f356030849
v.turkov <v.turkov@sysolutions.ru>
2020-08-03 17:05:28 +0700

    added filters and tc edition

--- src/app/ui/features-page/move-to-archive-dialog/MoveToArchiveDialog.ts
+++ src/app/ui/features-page/move-to-archive-dialog/MoveToArchiveDialog.ts
@@ -4,7 +4,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 @Component({
   selector: 'move-to-archive-dialog',
   template: `
-    <h4 mat-dialog-title>Сделать данную тех.возможность кекущей?</h4>
+    <h4 mat-dialog-title>Сделать данную тех.возможность текущей?</h4>
     <div mat-dialog-actions>
       <button mat-button (click)="onNoClick()">Отмена</button>
       <button mat-button [mat-dialog-close]="true" cdkFocusInitial>Да</button>
