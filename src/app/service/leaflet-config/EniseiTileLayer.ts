55812b944fc51368384a897f4e16832dc5349fb1
Aksanov Kirill <aksanovkv@cifra-k.ru>
2021-11-29 12:22:11 +0700

    Обновления с релизной ветки

--- src/app/service/leaflet-config/EniseiTileLayer.ts
+++ src/app/service/leaflet-config/EniseiTileLayer.ts
@@ -2,11 +2,11 @@ import {CRS, TileLayer} from 'leaflet';
 
 export class EniseiTileLayer extends TileLayer.WMS {
   constructor() {
-    super('http://map.24bpd.ru/geowebcache/service/wms?tiled=true', {
+    super('https://map.24bpd.ru/geowebcache/service/wms?tiled=true', {
       layers: 'egis_wld_light',
       format: 'image/png',
       transparent: false,
-      attribution: '© <a href="http://24bpd.ru/">Енисей-ГИС</a>',
+      attribution: '© <a href="https://24bpd.ru/">Енисей-ГИС</a>',
       crossOrigin: 'anonymous',
       crs: CRS.EPSG3857,
     });
