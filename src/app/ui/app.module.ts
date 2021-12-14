aca5df0ada77c2455598ec98d5dc2a17ed66fde8
Миляев Павел Александрович <milyaevpa@cifra-k.ru>
2021-12-14 16:38:07 +0700

    feat(best-map): возможность фильтрации по статусу точек подключения

--- src/app/ui/app.module.ts
+++ src/app/ui/app.module.ts
@@ -1,46 +1,46 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {ErrorHandler, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, Injector, NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {Menu} from './menu/menu';
-import {MapPage} from './map-page/map-page';
-import {Search} from './map-page/search/search';
-import {LocationInfoBar} from './map-page/location-info-bar/LocationInfoBar';
-import {OrganizationsInfoBar} from './map-page/organizations-info-bar/organizations-info-bar';
-import {BestMap} from './map-page/map/BestMap';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {LeafletModule} from '@asymmetrik/ngx-leaflet';
-import {LocationsPage} from './locations-page/locations-page';
-import {ServiceModule} from '@service/service.module';
-import {Authorization} from './authorization/authorization';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {FeaturesPage} from './features-page/features-page';
-import {OrganizationsPage} from './organizations-page/organizations-page';
-import {ContractsPage} from './contracts-page/contracts-page';
-import {LocationFiltersComponent} from './locations-page/location-filters/location-filters.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {InfoBar} from './map-page/info-bar/InfoBar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {InfiniteScrollModule} from 'ngx-infinite-scroll';
-import {MatSelectModule} from '@angular/material/select';
-import {OrganizationFilters} from './organizations-page/organization-filters/OrganizationFilters';
-import {Loader} from './loader/Loader';
-import {LoaderService} from './loader/LoaderService';
-import {LoaderServiceImpl} from './loader/LoaderServiceImpl';
-import {NgSelectModule} from '@ng-select/ng-select';
-import {UsersPage} from './users/users-page';
-import {JoinPipe} from './users/pipe/join.pipe';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {NZ_I18N, ru_RU} from 'ng-zorro-antd/i18n';
-import {registerLocaleData} from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Menu } from './menu/menu';
+import { MapPage } from './map-page/map-page';
+import { Search } from './map-page/search/search';
+import { LocationInfoBar } from './map-page/location-info-bar/LocationInfoBar';
+import { OrganizationsInfoBar } from './map-page/organizations-info-bar/organizations-info-bar';
+import { BestMap } from './map-page/map/BestMap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { LocationsPage } from './locations-page/locations-page';
+import { ServiceModule } from '@service/service.module';
+import { Authorization } from './authorization/authorization';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FeaturesPage } from './features-page/features-page';
+import { OrganizationsPage } from './organizations-page/organizations-page';
+import { ContractsPage } from './contracts-page/contracts-page';
+import { LocationFiltersComponent } from './locations-page/location-filters/location-filters.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { InfoBar } from './map-page/info-bar/InfoBar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { MatSelectModule } from '@angular/material/select';
+import { OrganizationFilters } from './organizations-page/organization-filters/OrganizationFilters';
+import { Loader } from './loader/Loader';
+import { LoaderService } from './loader/LoaderService';
+import { LoaderServiceImpl } from './loader/LoaderServiceImpl';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { UsersPage } from './users/users-page';
+import { JoinPipe } from './users/pipe/join.pipe';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NZ_I18N, ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
 import ru from '@angular/common/locales/ru';
 import {LocationPage} from './locations-page/location-page/location-page';
 import {MatCardModule} from '@angular/material/card';
@@ -132,8 +132,8 @@ registerLocaleData(ru);
 
 const toastrConfig = {
   positionClass: 'toast-bottom-right',
-  maxOpened: 3
-}
+  maxOpened: 3,
+};
 
 @NgModule({
   declarations: [
@@ -260,11 +260,7 @@ const toastrConfig = {
     NzDatePickerModule,
     ToastrModule.forRoot(toastrConfig),
   ],
-  exports: [
-    SortDirective,
-    CoreModule,
-    SharedModule
-  ],
+  exports: [SortDirective, CoreModule, SharedModule],
   providers: [
     LocationServiceOrganizationAccessPointsWithFilterParams,
     {
@@ -280,4 +276,5 @@ const toastrConfig = {
   ],
   bootstrap: [AppComponent],
 })
+
 export class AppModule {}
