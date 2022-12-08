2ca48d3259769f0cf68d40f415b9a5693dab3c3b
Сатышев Антон Сергеевич <satyshevas@cifra-k.ru>
2022-12-08 18:05:18 +0700

    ref::информация о точках подключения вынесена в отдельный компонент

---
+++ src/app/ui/old/shared/components/access-point-detail/access-point-detail.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Organization } from '@core/models';
+import { Reaccesspoint } from '@core/models/reaccesspoint';
+import { FomMonitoringWizardComponent } from '@shared/components/fom-monitoring-wizard/fom-monitoring-wizard.component';
+import { FormAccessPointComponent } from '@shared/components/form-access-point/form-access-point.component';
+import { NzModalService } from 'ng-zorro-antd';
+
+@Component({
+  selector: 'app-access-point-detail',
+  templateUrl: './access-point-detail.component.html',
+  styleUrls: ['./access-point-detail.component.scss']
+})
+export class AccessPointDetailComponent implements OnInit {
+
+  @Input() ap: Reaccesspoint;
+  @Input() organization: Organization;
+
+  constructor(
+
+    private modal: NzModalService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  initMonitoringAccessPoint(point: Reaccesspoint): void {
+    this.modal.create({
+      nzTitle: 'Подключить к системам мониторинга',
+      nzContent: FomMonitoringWizardComponent,
+      nzFooter: null,
+      nzComponentParams: {
+        accessPointForEdit: point,
+        organization: this.organization,
+      },
+    });
+  }
+
+  editAccessPoint(point: Reaccesspoint): void {
+    this.modal.create({
+      nzTitle: 'Редактирование точки доступа',
+      nzContent: FormAccessPointComponent,
+      nzFooter: null,
+      nzComponentParams: {
+        accessPointForEdit: point,
+        organization: this.organization,
+        mode: 'UPDATE'
+      },
+    });
+  }
+
+}
